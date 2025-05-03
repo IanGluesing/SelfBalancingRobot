94eb5d8f5b4c0e92e182c11330dee01987f5d87c
Ian Gluesing <ianmsgames@gmail.com>
2025-05-03 18:34:11 -0500

    Get encoder speed, use semaphore for concurrency control

--- get_gyro_data/get_gyro_data.ino
+++ get_gyro_data/get_gyro_data.ino
@@ -1,23 +1,41 @@
 #include "Wire.h"
 #include "MPU6050.h"
+#include "Motor.h"
 #include <Arduino_FreeRTOS.h>
+#include <semphr.h>
 
 MPU6050 mpu;
+Motor motor;
+
+int car_set_speed = 0;
 
 // Estimated current angle around the X axis
 float angleAroundXAxis;
 float gyro_data_task_dt_milliseconds = 5;
 void gyro_data_task(void *pvParameters);
 
+SemaphoreHandle_t encoder_mutex;
+int encoder_speed_left = 0;
+int encoder_speed_right = 0;
+float get_encoder_speed_task_dt_milliseconds = 5;
+void get_encoder_speed(void *pvParameters);
+
 void setup() {
+  motor.Pin_init();
+  motor.Encoder_init();
   Serial.begin(115200);
   while(!Serial) {}
   
   Wire.begin();
   mpu.initialize();
 
+  encoder_mutex = xSemaphoreCreateMutex();
+
   // Spawn task to get gyro data
   xTaskCreate(gyro_data_task, "Gyro Data Task", 128, NULL, 1, NULL);
+
+  // Spawn task for speed control output
+  xTaskCreate(get_encoder_speed, "Speed control task", 128, NULL, 1, NULL);
 }
 
 void gyro_data_task(void *pvParameters) {
@@ -42,10 +60,29 @@ void gyro_data_task(void *pvParameters) {
   
     // To determine the actual final angle, use simplified complimentary filter
     angleAroundXAxis = (1 - weight_factor) * (angleAroundXAxis + rotationalVelocityAroundXAxis * (gyro_data_task_dt_milliseconds / 1000)) + (weight_factor * angleOfGravityFromZAxis);
-    
-    Serial.println("angleAroundXAxis: " + String(angleAroundXAxis));
+
+    if (xSemaphoreTake(encoder_mutex, portMAX_DELAY) == pdTRUE) {
+      Serial.println("encoder_speed_left: " + String(encoder_speed_left) + " encoder_speed_right: " + String(encoder_speed_right));
+      xSemaphoreGive(encoder_mutex);
+    }
 
     vTaskDelay(gyro_data_task_dt_milliseconds / portTICK_PERIOD_MS);
+  }
+}
+
+void get_encoder_speed(void *pvParameters) {
+  (void) pvParameters;
+
+  while (true) {
+    if (xSemaphoreTake(encoder_mutex, portMAX_DELAY) == pdTRUE) {
+      encoder_speed_left += (car_set_speed < 0) ? (-Motor::encoder_count_left_a) : (Motor::encoder_count_left_a);
+      encoder_speed_right += (car_set_speed < 0) ? (-Motor::encoder_count_right_a) : (Motor::encoder_count_right_a);
+      Motor::encoder_count_left_a = 0;
+      Motor::encoder_count_right_a = 0;
+
+      xSemaphoreGive(encoder_mutex);
+    }
+    vTaskDelay(get_encoder_speed_task_dt_milliseconds / portTICK_PERIOD_MS);
   }
 }
 
